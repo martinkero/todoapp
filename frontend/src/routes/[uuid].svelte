799e281a4f23babd7f61516f9dbcde83a6d76b8e
Martin Kero <martin.kero@gmail.com>
2021-11-16 10:15:43 +0100

    Removed prerender bool

--- frontend/src/routes/[uuid].svelte
+++ frontend/src/routes/[uuid].svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import TodoList from '$lib/todo/TodoList.svelte';
 	import { page } from '$app/stores';
-	export const prerender = true;
 
 	const uuid = $page.params.uuid;
 </script>
